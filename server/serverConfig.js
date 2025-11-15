33f5e269fa14bc0f1782fa1b7a8ddf0f105eef51
Natalie Markova <charmed-n@mail.ru>
2025-11-15 18:12:00 +0300

    Update serverConfig.js

--- server/serverConfig.js
+++ server/serverConfig.js
@@ -6,18 +6,10 @@ module.exports = (app) => {
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }));
     app.use(cookieParser());
-    const allowedOrigins = process.env.CLIENT_URL 
-        ? process.env.CLIENT_URL.split(',').map(url => url.trim())
-        : ["http://localhost:3000"];
-    
+
+ 
     app.use(cors({
-        origin: (origin, callback) => {
-            if (!origin || allowedOrigins.includes(origin)) {
-                callback(null, true);
-            } else {
-                callback(null, true);
-            }
-        },
-        credentials: true
+        origin: "*",      
+        credentials: true  
     }));
 };
