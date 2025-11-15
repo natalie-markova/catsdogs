ba5f35b0141871e86d4bd98dacd295beaec75fc6
Natalie Markova <charmed-n@mail.ru>
2025-11-15 18:00:26 +0300

    Update axios.instance.ts

--- client/src/utils/axios.instance.ts
+++ client/src/utils/axios.instance.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5001';
+const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5001'; 
 
 export const $api = axios.create({
-   baseURL: 'https://catsdogs-backend-rqo8.onrender.com',
+   baseURL: API_URL, 
    withCredentials: true
 });
